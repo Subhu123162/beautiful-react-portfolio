d0dd59b6c1ce3c05a93049a6d9b3070eb371cc13
subasish jana <subasishjana36@gmail.com>
2025-07-29 21:45:35 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillSection } from "../components/SkillSection";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { ContactSeaction } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        <ThemeToggle />
+        <StarBackground />
+        <Navbar />
+        <main>
+          <HeroSection />
+          <AboutSection />
+          <SkillSection />
+          <ProjectsSection />
+          <ContactSeaction />
+          <Footer />
+        </main>
+    </div>
+  );
+};
