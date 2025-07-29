3d1a847e24e6c5a3d1eab46140c89ca15bd0c639
subasish jana <subasishjana36@gmail.com>
2025-07-29 23:34:32 +0530

    Project solved and updated with new features

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -7,27 +7,27 @@ import {
   Send,
   Twitch,
   Twitter,
+  Facebook,
 } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useToast } from "../hooks/use-toast";
 import { useState } from "react";
 export const ContactSeaction = () => {
-    const { toast } = useToast();
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const { toast } = useToast();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        setIsSubmitting(true);
+    setIsSubmitting(true);
 
-        setTimeout(() => {
-            toast({
-                title: "Message Sent!",
-                description: "Thank you for your message. I'll get back to you soon.",
-            });
-            setIsSubmitting(false);
-        },1500)
-        
-    }
+    setTimeout(() => {
+      toast({
+        title: "Message Sent!",
+        description: "Thank you for your message. I'll get back to you soon.",
+      });
+      setIsSubmitting(false);
+    }, 1500);
+  };
   return (
     <section id="contact" className="py-24 px-4 relative bg-secondary/30">
       <div className="container mx-auto max-w-5xl">
@@ -89,27 +89,57 @@ export const ContactSeaction = () => {
             <div className="pt-8">
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
-                  <Linkedin />
+                <a
+                  href="https://www.linkedin.com/in/subasish-jana-7964722bb"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Linkedin className="hover:text-primary transition-colors" />
                 </a>
-                <a href="#" target="_blank">
-                  <Twitter />
+                <a
+                  href="https://x.com/subasishjana?t=Xj0X7-XombQ1XxB5Bq7Z8g&s=09"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Twitter className="hover:text-primary transition-colors" />
                 </a>
-                <a href="#" target="_blank">
-                  <Instagram />
+
+                <a
+                  href="https://www.instagram.com/subasish_4274"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Instagram className="hover:text-primary transition-colors" />
                 </a>
+                <a
+                  href="https://www.facebook.com/share/1AuAGssK5Q/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Facebook className="text-xl hover:text-primary transition-colors" />
+                </a>
+
                 <a href="#" target="_blank">
                   <Twitch />
                 </a>
               </div>
             </div>
           </div>
-          <div className="bg-card p-8 rounded-lg shadow-xs" onSubmit={handleSubmit}>
+          <div
+            className="bg-card p-8 rounded-lg shadow-xs"
+            onSubmit={handleSubmit}
+          >
             <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
             <form lassName="space-y-6">
               <div>
-                <leble htmlFor="name" className ="block text-sm font-medium mb-2"> Your Name</leble>
-               <input
+                <leble
+                  htmlFor="name"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Name
+                </leble>
+                <input
                   type="text"
                   id="name"
                   name="name"
@@ -118,9 +148,15 @@ export const ContactSeaction = () => {
                   placeholder="Your Name..."
                 />
               </div>
-                <div>
-                <leble htmlFor="email" className ="block text-sm font-medium mb-2"> Your Email</leble>
-               <input
+              <div>
+                <leble
+                  htmlFor="email"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Email
+                </leble>
+                <input
                   type="email"
                   id="email"
                   name="email"
@@ -129,9 +165,15 @@ export const ContactSeaction = () => {
                   placeholder="Your EmailId..."
                 />
               </div>
-                <div>
-                <leble htmlFor="message" className ="block text-sm font-medium mb-2"> Your Message</leble>
-               <textarea
+              <div>
+                <leble
+                  htmlFor="message"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Message
+                </leble>
+                <textarea
                   id="message"
                   name="message"
                   required
@@ -141,7 +183,7 @@ export const ContactSeaction = () => {
               </div>
               <button
                 type="submit"
-                disabled = {isSubmitting}
+                disabled={isSubmitting}
                 className={cn(
                   "cosmic-button w-full flex items-center justify-center gap-2"
                 )}
